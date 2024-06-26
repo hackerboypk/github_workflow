ede090c131068e8dbb21c2e3cf86b83443d0fe90
hackerboypk <43108239+hackerboypk@users.noreply.github.com>
2024-06-26 13:59:44 +0530

    Create validation.py

---
+++ validation.py
@@ -0,0 +1,8 @@
+import logging 
+logging.basicConfig(level=logging.INFO) 
+logger = logging.getLogger(__name__) 
+
+def main():
+    logger.info("Successfully validated") 
+if name == "__main__": 
+    main()
